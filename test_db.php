42753be7e998ee70de94248dd8208b092265a59d
JeremyPanaiva <jeremy.panaiva@etu.univ-amu.fr>
2025-09-30 20:17:35 +0200

    test sql

---
+++ test_db.php
@@ -0,0 +1,27 @@
+<?php
+// Connexion à la base Alwaysdata
+$servername = "mysql.alwaysdata.com";  // host Alwaysdata
+$username = "432905_jeremy";
+$password = "saemanager-g4!";            // ton mot de passe MySQL
+$dbname = "sae-manager-g4_db";
+
+// Créer la connexion
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Vérifier la connexion
+if ($conn->connect_error) {
+    die("Connexion échouée: " . $conn->connect_error);
+}
+echo "Connexion réussie !<br>";
+
+// Tester : récupérer tous les utilisateurs
+$result = $conn->query("SELECT id, nom, prenom, mail FROM users");
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo $row['id'] . " - " . $row['prenom'] . " " . $row['nom'] . " <" . $row['mail'] . "><br>";
+    }
+} else {
+    echo "Aucun utilisateur trouvé.";
+}
+?>
