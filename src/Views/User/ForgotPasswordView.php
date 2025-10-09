6e5c8d2a0d2152c3b9210e95956343c5f9d8a9a6
Mohamed_D <mohamed.driouchi@etu.univ-amu.fr>
2025-10-09 16:11:40 +0200

    vue mot de passe oublié

--- src/Views/User/ForgotPasswordView.php
+++ src/Views/User/ForgotPasswordView.php
@@ -1,20 +1,18 @@
 <?php
 
 namespace Views\User;
+
 use Views\Base\BaseView;
 
 class ForgotPasswordView extends BaseView {
 
-    public const EMAIL = "email";
-    public const EMAIL_KEY = 'EMAIL_KEY';
-    private const KEYS = [self::EMAIL_KEY => self::EMAIL];
     private const TEMPLATE_HTML = __DIR__ . '/forgot-password.html';
 
-    public function templatePath() : string {
+    public function templatePath(): string {
         return self::TEMPLATE_HTML;
     }
 
-    public function templateKeys() : array {
-        return self::KEYS;
+    public function templateKeys(): array {
+        return [];
     }
 }
